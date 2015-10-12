738c18252d104904ad99fac96f26c8dc2547aa29
Dennis Smits <info@dennissmits.nl>
2015-10-13 01:32:45 +0200

    Module 4 homework
    My homework for module 4.

---
+++ RestaurantManager.UniversalWindows/RestaurantManager.Models/DataManager.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace RestaurantManager.Models
+{
+    public class DataManager
+    {
+        public DataManager()
+        {
+            OrderItems = new ObservableCollection<string>(
+                new List<string>
+                {
+                    "Steak, Chicken, Peas",
+                    "Rice, Chicken",
+                    "Hummus, Pita"
+                }
+            );
+
+            MenuItems = new List<string>
+            {
+                "Steak",
+                "Chicken",
+                "Peas",
+                "Rice",
+                "Hummus",
+                "Pita"
+            };
+
+            CurrentlySelectedMenuItems = new List<string>
+            {
+                "Rice",
+                "Pita"
+            };
+
+        }
+
+        public ObservableCollection<string> OrderItems { get; set; }
+        public List<string> MenuItems { get; set; }
+        public List<string> CurrentlySelectedMenuItems { get; set; }
+    }
+}
+
